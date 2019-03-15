6b1c6bc4967e64438babed549dc027620541c53d
金澤 清 <kazayoshi001@gmail.com>
2019-03-15 14:05:01 +0900

    initial commit

---
+++ src/main/kotlin/com/tklcraft/twipostmc/TwipostMCPlugin.kt
@@ -0,0 +1,15 @@
+package com.tklcraft.twipostmc
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TwipostMCPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
