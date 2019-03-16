870785570851e90285ff4f5e9a183762772c729e
金澤 清 <kazayoshi001@gmail.com>
2019-03-17 00:30:10 +0900

    add .gradle/ to gitignore

---
+++ build/libs/debug.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+set plugin_path="C:\Users\kiyoshi kanazawa\Desktop\miecraft\plugins"
+set server_path="C:\Users\kiyoshi kanazawa\Desktop\miecraft"
+
+copy /Y TwipostMC-1.0-SNAPSHOT.jar %plugin_path%
+cd %server_path%
+java -Xms1024M -Xmx1024M -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 spigot-1.13.2.jar
