44c14650edb78c8f93cf0cad8c0a382175d1a78e
金澤 清 <kazayoshi001@gmail.com>
2019-03-30 23:27:53 +0900

    ビルド設定がおかしくなってたので、修正

--- src/main/kotlin/com/tklcraft/twipostmc/command/CommandList.kt
+++ src/main/kotlin/com/tklcraft/twipostmc/command/CommandList.kt
@@ -1,8 +1,7 @@
 package com.tklcraft.twipostmc.command
 
 const val TW_CMD = "tw"
-const val POST_CMD = "post"
 const val REGISTER_CMD = "register"
 const val PIN_CMD = "pin"
+const val POST_CMD = "post"
 const val DEBUG_CMD = "debug"
-const val SAMPLE_CMD = "sample"
