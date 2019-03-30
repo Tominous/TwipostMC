6888fb951d5c8696e02db4127ac71d3765025b9e
金澤 清 <kazayoshi001@gmail.com>
2019-03-30 22:36:16 +0900

    gitignore更新

--- build/libs/debug.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-set plugin_path="C:\Users\kiyoshi kanazawa\Documents\miecraft_server\plugins"
-set server_path="C:\Users\kiyoshi kanazawa\Documents\miecraft_server"
-
-copy /Y TwipostMC-1.0-SNAPSHOT.jar %plugin_path%
-cd %server_path%
-java -Xms1024M -Xmx1024M -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 spigot-1.13.2.jar
